8b9e18c7a291984885e0218f27e5e7dd1a879348
Gregory Mitrano <gregory.mitrano@gmail.com>
2018-11-26 00:25:10 -0500

    Added initial C FFI functions + generated bindings for websocket server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,6 +2,13 @@ use failure::Error;
 
 use forge::server::run_server;
 
+use std::ptr;
+use std::thread;
+use std::time::Duration;
+
 fn main() -> Result<(), Error> {
-    run_server()
+    let context = run_server("127.0.0.1", 8005, &forge::server::PacketCallback {userdata: ptr::null_mut(), func: None })?;
+    thread::sleep(Duration::from_secs(3));
+    context.shutdown()?;
+    Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,37 +1,65 @@
 use failure::Error;
 
-use ws::{listen, Handler, Sender, Message, CloseCode, Handshake, Response, Request};
+use ws::{CloseCode, Handler, Handshake, Message, Request, Response, Sender};
 
-use std::ffi::c_void;
+use std::os::raw::c_void;
+use std::ptr;
+use std::thread;
+use std::ffi::CStr;
+use std::os::raw::c_char;
 
 static FORGE_PROTOCOL: &str = "forge";
 
-#[repr(C)]
-struct PacketCallback {
-    userdata: *mut c_void,
-    func: Option<unsafe extern "C" fn(*mut c_void, *const c_void)>
+struct ForgeHandler {
+    client_sender: Sender,
+    callback: PacketCallback
 }
 
-pub struct Server {
-    pub out: Sender,
-}
-
-impl Server {
+impl ForgeHandler {
     fn process_packet(&mut self, packet: &Vec<u8>) {
         println!("Received {} byte packet", packet.len());
+        unsafe {
+            match self.callback.func {
+                Some(func) => func(self.callback.userdata, std::mem::transmute(packet.as_ptr())),
+                None => ()
+            }
+        }
     }
 }
 
-impl Handler for Server {
+pub struct ServerContext {
+    sender: Sender,
+    thread: thread::JoinHandle<()>
+}
+
+impl ServerContext {
+    pub fn shutdown(self) -> Result<(), Error> {
+        println!("Server Shutting Down...");
+        self.sender.shutdown()?;
+        // @TODO: Maybe handle this differently?
+        self.thread.join().unwrap();
+        println!("Server Shut Down");
+        Ok(())
+    }
+}
+
+impl Handler for ForgeHandler {
     fn on_request(&mut self, req: &Request) -> ws::Result<Response> {
-        if req.protocols().unwrap().iter().find(|proto| { proto == &&FORGE_PROTOCOL }).is_some() {
+        if req
+            .protocols()
+            .unwrap()
+            .iter()
+            .find(|proto| proto == &&FORGE_PROTOCOL)
+            .is_some()
+        {
             let mut resp = Response::from_request(req)?;
             resp.set_protocol(FORGE_PROTOCOL);
             Ok(resp)
-        }
-        else
-        {
-            Err(ws::Error::new(ws::ErrorKind::Protocol, format!("'{}' protocol not found", FORGE_PROTOCOL)))
+        } else {
+            Err(ws::Error::new(
+                ws::ErrorKind::Protocol,
+                format!("'{}' protocol not found", FORGE_PROTOCOL),
+            ))
         }
     }
 
@@ -42,21 +70,25 @@ impl Handler for Server {
 
     fn on_message(&mut self, msg: Message) -> ws::Result<()> {
         match msg {
-            Message::Binary(packet) => {
-                Ok(self.process_packet(&packet))
-            },
-            Message::Text(_) => Err(ws::Error::new(ws::ErrorKind::Protocol, "Text data not supported by protocol"))
+            Message::Binary(packet) => Ok(self.process_packet(&packet)),
+            Message::Text(_) => Err(ws::Error::new(
+                ws::ErrorKind::Protocol,
+                "Text data not supported by protocol",
+            )),
         }
     }
 
     fn on_close(&mut self, code: CloseCode, reason: &str) {
         match code {
             CloseCode::Normal => println!("The client is done with the connection."),
-            CloseCode::Away   => println!("The client is leaving the site."),
+            CloseCode::Away => println!("The client is leaving the site."),
             CloseCode::Status => println!("The client closed the socket."),
-            _ => println!("The client encountered an error: {:#?}, reason: {}", code, reason),
+            _ => println!(
+                "The client encountered an error: {:#?}, reason: {}",
+                code, reason
+            ),
         }
-        self.out.shutdown().unwrap();
+        self.client_sender.shutdown().unwrap();
     }
 
     fn on_error(&mut self, err: ws::Error) {
@@ -64,8 +96,69 @@ impl Handler for Server {
     }
 }
 
-pub fn run_server() -> Result<(), Error> {
-  println!("Starting server");
-  listen("127.0.0.1:8005", |out| Server { out: out } )?;
-  Ok(())
+pub fn run_server(host: &str, port: u32, callback: &PacketCallback) -> Result<ServerContext, Error> {
+    println!("Server Starting...");
+
+    let callback = callback.clone();
+    let mut settings = ws::Settings::default();
+    settings.max_connections = 1;
+    let socket = ws::Builder::new()
+        .with_settings(settings)
+        .build(move |sender: ws::Sender| {
+            ForgeHandler { client_sender: sender, callback: callback.clone() }
+        }).unwrap();
+
+    let server_socket = socket.broadcaster();
+
+    let address = format!("{}:{}", host, port);
+
+    let server_thread = thread::spawn(move || {
+        println!("Server Listening on {}", address);
+        socket.listen(address).unwrap();
+    });
+
+    Ok(ServerContext { sender: server_socket, thread: server_thread })
+}
+
+#[repr(C)]
+#[derive(Clone)]
+pub struct PacketCallback {
+    pub userdata: *mut c_void,
+    pub func: Option<unsafe extern "C" fn(*mut c_void, *const c_void)>,
+}
+
+// @TODO Is this correct...?
+unsafe impl Send for PacketCallback {}
+unsafe impl Sync for PacketCallback {}
+
+#[repr(C)]
+pub struct ServerConfig {
+    pub host: Option<*const c_char>,
+    pub callback: PacketCallback,
+    pub port: u32,
+    _padding : u32
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn forge_start_server(config: *const ServerConfig) -> *mut ServerContext {
+    if config.is_null() { return ptr::null_mut(); }
+
+    let config = &(*config);
+    if config.host.is_none() { return ptr::null_mut(); }
+    if config.callback.func.is_none() { return ptr::null_mut(); }
+
+    match run_server(&CStr::from_ptr(config.host.unwrap()).to_string_lossy(), config.port, &config.callback) {
+        Ok(context) => {
+            Box::into_raw(Box::new(context))
+        },
+        Err(_) => ptr::null_mut()
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn forge_stop_server(context: *mut ServerContext) {
+    if context.is_null() {
+        let context = Box::from_raw(context);
+        context.shutdown().unwrap();
+    }
 }
